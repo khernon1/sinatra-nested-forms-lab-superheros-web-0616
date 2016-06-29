dc6c628d06b066bb5be80209b8baa10ce242eb8c
kevin hernon <kevinhernon@kevins-MacBook-Air.local>
2016-06-28 20:35:54 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do      
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @motto = params[:team][:motto]
+      erb :team
+       #binding.pry
+    end
 
 end
